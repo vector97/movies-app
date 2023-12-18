68bb673722615e28f79518508a9c8477ef8ac569
Viktor Bogatenkov <vector_97@mail.ru>
2023-12-18 17:38:13 +0400

    feat: добавил обработку отсутствия интернета

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import App from './components/App'
+import Error from './components/Error'
 
+import { Offline, Online } from 'react-detect-offline'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <div>
+    <Online>
+      <App />
+    </Online>
+    <Offline>
+      <Error error={{ message: 'Internet Disconnected' }} />
+    </Offline>
+  </div>
+)
